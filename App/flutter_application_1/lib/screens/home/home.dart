3eb1638ca48d8088d79c99f7205141794cf9a3d8
omarshafik12 <omarsaidshafik@gmail.com>
2025-03-24 10:10:07 -0400

    copied feature

--- App/flutter_application_1/lib/screens/home/home.dart
+++ App/flutter_application_1/lib/screens/home/home.dart
@@ -101,64 +101,112 @@ class HomePage extends StatelessWidget {
         ],
       ),
       extendBody: true,
-      floatingActionButton: FloatingActionButton(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-        backgroundColor: Colors.white,
-        onPressed: () {Navigator.pushNamed(context, '/homepage');
-              },
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        height: 50,
-        color: Colors.white,
-        shape: const CircularNotchedRectangle(),
-        notchMargin: 10,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            IconButton(
-              icon: SizedBox(
-                width: 32,
-                height: 32,
-                child: Image.asset('assets/home_page/icons/price-tag.png'),
+      bottomNavigationBar: Material(
+              color: Colors.transparent,
+              elevation: 100,
+              child: ClipPath(
+                child: SizedBox(
+                  child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          IconButton(
+                              icon: Icon(
+                                Icons.home,
+                                color: Colors.deepPurple,
+                                size: 30,
+                              ),
+                              onPressed: () {
+                                setState() {}
+                              }),
+                          IconButton(
+                              icon: Icon(
+                                Icons.branding_watermark,
+                                size: 30,
+                                color: Colors.black54,
+                              ),
+                              onPressed: () {}),
+                          SizedBox(
+                            width: 50,
+                          ),
+                          IconButton(
+                              icon: Icon(
+                                Icons.cloud_download,
+                                size: 30,
+                                color: Colors.black54,
+                              ),
+                              onPressed: () {}),
+                          IconButton(
+                              icon: Icon(
+                                Icons.face,
+                                size: 30,
+                                color: Colors.black54,
+                              ),
+                              onPressed: () {}),
+                        ],
+                      )),
+                  height: 100,
+                  width: double.infinity,
+                ),
+                clipper: CurveDraw(),
               ),
-              onPressed: () {Navigator.pushNamed(context, '/homepage');
-              },
             ),
-            IconButton(
-              icon: SizedBox(
-                width: 32,
-                height: 32,
-                child: Image.asset('assets/home_page/icons/price-tag.png'),
+            floatingActionButton: MaterialButton(
+              color: Colors.deepPurple,
+              padding: EdgeInsets.all(20),
+              onPressed: () {},
+              shape: CircleBorder(),
+              child: Icon(
+                Icons.add,
+                size: 35,
+                color: Colors.white,
               ),
-              onPressed: () {Navigator.pushNamed(context, '/homepage');
-              },
             ),
-            IconButton(
-              icon: SizedBox(
-                width: 32,
-                height: 32,
-                child: Image.asset('assets/home_page/icons/price-tag.png'),
-              ),
-              onPressed: () {Navigator.pushNamed(context, '/homepage');
-              },
-            ),
-            IconButton(
-              icon: SizedBox(
-                width: 32,
-                height: 32,
-                child: Image.asset('assets/home_page/icons/price-tag.png'),
-              ),
-              onPressed: () {Navigator.pushNamed(context, '/homepage');
-              },
-            ),
-          ],
-        ),
-      ),
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+}
+
+class CurveDraw extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    double sw = size.width;
+    double sh = size.height;
+    double gapConst = 50;
+    print(sh);
+    Path path = Path();
+    path.moveTo(0, sh);
+    path.lineTo(0, sh / 2);
+    path.quadraticBezierTo(0, 0, sh / 2, 0); //1st curve
+    path.lineTo(sw / 2 - sw / 5, 0);
+    path.cubicTo(sw / 2 - sw / 8, 0, sw / 2 - sw / 8, sh / 2, sw / 2, sh / 2);
+    path.cubicTo(
+        sw / 2 + sw / 8, sh / 2, sw / 2 + sw / 8, 0, sw / 2 + sw / 5, 0);
+
+    path.lineTo(sw - sh / 2, 0);
+
+    // path.quadraticBezierTo(
+    //     150, size.height - 100, 150, size.height - 110); //2nd Curve IMP
+    // path.lineTo(size.width - 150, size.height - 100); //draw semi circle
+    // path.quadraticBezierTo(size.width - 150, size.height - 100, size.width - 90,
+    //     size.height - 100);
+    // path.lineTo(size.width - 60, size.height - 100);
+    path.quadraticBezierTo(sw, 0, size.width, sh / 2);
+    path.lineTo(sw, sh);
+    // path.quadraticBezierTo(
+    //     size.width / 2 - 50, size.height / 3, 2 * size.width / 3, 0);
+    //path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) {
+    return true;
   }
 }
 
